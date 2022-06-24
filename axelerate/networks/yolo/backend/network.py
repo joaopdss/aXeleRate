3484bd4aa7b7159f485db3f9660ce54f9c857b8e
João Pedro dos Santos <72708214+joaopdss@users.noreply.github.com>
2022-06-24 10:46:03 -0300

    Update network.py

--- axelerate/networks/yolo/backend/network.py
+++ axelerate/networks/yolo/backend/network.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 import os
 import time
+import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Reshape, Conv2D, Input, Lambda
 from axelerate.networks.common_utils.feature import create_feature_extractor
@@ -35,7 +36,7 @@ class YoloNetwork(object):
         output_tensor = Conv2D(nb_box * (4 + 1 + nb_classes), (1,1), strides=(1,1),
                                padding='same', 
                                name='detection_layer_{}'.format(nb_box * (4 + 1 + nb_classes)), 
-                               kernel_initializer='lecun_normal')(feature_extractor.feature_extractor.outputs[0])
+                             kernel_initializer='lecun_normal', dtype=tf.float32)(feature_extractor.feature_extractor.outputs[0])
         
         output_tensor = Reshape((grid_size_x, grid_size_y, nb_box, 4 + 1 + nb_classes))(output_tensor)
         print(f"Output tensor: {output_tensor}")
