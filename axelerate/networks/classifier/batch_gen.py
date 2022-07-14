bcd2ff1c8a22b55f24101398829fb45317c9058d
João Pedro dos Santos <72708214+joaopdss@users.noreply.github.com>
2022-07-14 15:45:37 -0300

    Update batch_gen.py

--- axelerate/networks/classifier/batch_gen.py
+++ axelerate/networks/classifier/batch_gen.py
@@ -199,6 +199,6 @@ class ImageDataAugmentor(Sequence):
         image = self.process_image(image, desired_w, desired_h, self.augment)
 	
 				if self.preprocess_input != None:
-        	image = self.preprocess_input(image)
+			image = self.preprocess_input(image)
 		
         return image
