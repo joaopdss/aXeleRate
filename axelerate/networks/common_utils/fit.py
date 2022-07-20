7ec7b7a64a892a794f560bf1893b00b58bb80e01
João Pedro dos Santos <72708214+joaopdss@users.noreply.github.com>
2022-07-20 16:15:23 -0300

    Update fit.py

--- axelerate/networks/common_utils/fit.py
+++ axelerate/networks/common_utils/fit.py
@@ -160,8 +160,8 @@ def get_class_weights(train_batch_gen, project_folder):
 	class_weights = {}
 	for key, val in train_batch_gen.class_indices.items():
 		amount_imgs = len(os.listdir(os.path.join(project_folder, key)))
-		print(train_batch_gen)
 		print(train_batch_gen[0])
+# 		print(train_batch_gen[0])
 # 		print(train_batch_gen.shape)
 # 		class_weights[val] = (1 / amount_imgs) * ((len(train_batch_gen) * ) / 2.0)
 	
