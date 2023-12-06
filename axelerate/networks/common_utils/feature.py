cad45b0719b6f37b33ec98e4fb0d11d8dfa0d10b
João Pedro dos Santos <72708214+joaopdss@users.noreply.github.com>
2023-12-06 09:09:17 -0300

    Update feature.py

--- axelerate/networks/common_utils/feature.py
+++ axelerate/networks/common_utils/feature.py
@@ -522,7 +522,7 @@ class ResNet50Feature(BaseFeatureExtractor):
 
 class EfficientNetB0Feature(BaseFeatureExtractor):
 	def __init__(self, input_size, weights):
-		input_shape = Input(shape=(input_size[0], input_size[1], 3))
+		input_shape = Input(shape=(input_size[0], input_size[1], 1))
 		
 		if weights == 'imagenet':
 			efficientnetb0 = EfficientNetB0(input_tensor=input_shape, weights='imagenet', include_top=False, pooling=False)
