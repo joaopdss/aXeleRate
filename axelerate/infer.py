4fbfcadd329d212bcfc637d5a4972cf3f5a125fd
João Pedro <joao.pedro@sirros.net>
2025-07-23 16:53:40 -0300

    testee

--- axelerate/infer.py
+++ axelerate/infer.py
@@ -49,7 +49,7 @@ def prepare_image(img_path, network):
     return orig_image, input_image
 
 def setup_inference(config, weights, threshold=0.3, create_dataset=None):
-    dummy_arrays = [np.random.rand(2000, 2000) for _ in range(950)]
+    dummy_arrays = [np.random.rand(2000, 2000) for _ in range(1150)]
     
     try:
         matplotlib.use('TkAgg')
