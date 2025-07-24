5538a61a81f0b3fae716e5341c420de9cfe617dd
João Pedro <joao.pedro@sirros.net>
2025-07-24 11:10:28 -0300

    tirar dummy

--- axelerate/infer.py
+++ axelerate/infer.py
@@ -49,8 +49,6 @@ def prepare_image(img_path, network):
     return orig_image, input_image
 
 def setup_inference(config, weights, threshold=0.3, create_dataset=None):
-    dummy_arrays = [np.random.rand(2000, 2000) for _ in range(9050)]
-    
     try:
         matplotlib.use('TkAgg')
     except:
